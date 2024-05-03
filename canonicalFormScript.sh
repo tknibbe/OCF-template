3dbc92c0f1af1829a193bbd20d0a286b21e0eee5
tymon knibbe <tymonknibbe@tymons-MacBook-Pro.local>
2024-05-03 13:08:43 +0200

    addded canonical form script

---
+++ canonicalFormScript.sh
@@ -0,0 +1,60 @@
+#/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, please give the name of the class you'd like to create"
+	exit
+fi
+
+
+className=$1
+
+touch "$className.cpp"
+touch "$className.hpp"
+
+echo "#pragma once
+
+class $className {
+	private:
+
+	protected:
+
+	public:
+		$className();
+		~$className();
+		$className(const $className& other);
+		$className&	operator=(const $className& other);
+};" > "$className.hpp"
+
+
+echo "#include \"$className.hpp\"
+#include <iostream>
+
+$className::$className(void){
+	std::cout << \"Default constructor called\" << std::endl;
+	/*
+		set default values
+	*/
+}
+
+$className::~$className(void){
+	std::cout << \"Destructor called\" << std::endl;
+}
+
+$className::$className(const $className& ref){
+	std::cout << \"Copy constructor called\" << std::endl;
+	*this = ref;
+}
+
+$className& $className::operator=(const $className& ref){
+	std::cout << \"Copy assignment operator called\" << std::endl;
+	if (this != &ref)
+	{
+		/*
+		specify instructions here
+		*/
+	}
+	return *this;
+}" > "$className.cpp"
+
+echo "creating orthodox canonical form for class: $className"
